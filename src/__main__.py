0f890f69a9c0efe4fae3fe1dd3c019d4835a3739
Rohan Sarvaria <rsarvaria@uwaterloo.ca>
2024-04-08 10:49:56 -0400

    feat(main): allow for alternate config locations as command line parameters

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,19 @@
+import argparse
 import dotenv
-
-dotenv.load_dotenv(".env", override=True)
-
-import bureaucrat
-import os
-
 
 def main():
 
-    config = bureaucrat.Config.load("config.toml")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help="path to a config.toml file", default="config.toml")
+    parser.add_argument("--env", help="path to a .env file", default=".env")
+    args = parser.parse_args()
+
+    dotenv.load_dotenv(args.env, override=True)
+
+    import bureaucrat
+    import os
+
+    config = bureaucrat.Config.load(args.config)
     bot = bureaucrat.Bureaucrat(config=config)
     bot.run(os.getenv("DISCORD_TOKEN"))
 
